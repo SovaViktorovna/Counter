8241cfba9bceb1a70b63d6e8810ed6d56d6cffb8
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-02-25 10:04:37 +0400

    added a counter button and output the value to the label

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    public var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func increaseСounter(_ sender: Any) {
+        counter += 1
+        counterValue.text = String(counter)
+    }
+    
+    @IBOutlet weak var counterValue: UILabel!
 }
 
